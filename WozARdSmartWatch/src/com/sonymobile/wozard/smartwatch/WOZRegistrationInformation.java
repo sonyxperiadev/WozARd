d2cc50f82462bf549180b69bb1a6e82e14eacc17
gunAlc <Gunter.Alce@sonymobile.com>
2014-06-24 14:42:50 +0200

    Renamed Smartwatch package name.
    Renamed Smartwatch package name.

--- WozARdSmartWatch/src/com/sonymobile/wozard/smartwatch/WOZRegistrationInformation.java
+++ WozARdSmartWatch/src/com/sonymobile/wozard/smartwatch/WOZRegistrationInformation.java
@@ -29,7 +29,7 @@
  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package com.sonyericsson.extras.liveware.extension.controlsample;
+package com.sonymobile.wozard.smartwatch;
 
 
 import android.content.ContentValues;
